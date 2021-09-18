21c00f4e93759df659b1462c7d4b5ff2be61d421
Rodeobazar124 <Azfasa15@gmail.com>
2021-09-18 18:36:59 +0700

    certus: Set LCD Density in product props

--- product_prop.mk
+++ product_prop.mk
@@ -42,3 +42,6 @@ PRODUCT_PRODUCT_PROPERTIES  += \
    ro.surface_flinger.vsync_sf_event_phase_offset_ns=830000 \
    ro.surface_flinger.present_time_offset_from_vsync_ns=0
 
+# LCD Density
+PRODUCT_PRODUCT_PROPERTIES  += \
+   ro.sf.lcd_density=320
